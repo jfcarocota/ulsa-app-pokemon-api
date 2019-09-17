5a91d5197f4e2575523827e063730b867f1a1979
jfcarocota <jfcarocota@gmail.com>
2019-09-17 13:53:07 -0700

    pokemon fetch

---
+++ app.js
@@ -0,0 +1,15 @@
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonHeight = document.getElementById('pokemon-height');
+
+const pokemonApiUrl = 'https://pokeapi.co/api/v2/';
+
+let getPokemonData = async ()=> {
+    const respose = await fetch(`${pokemonApiUrl}pokemon/1`);
+    const pokemon = await respose.json();
+    console.log(pokemon);
+    const {name , height} = pokemon;
+    pokemonName.innerText = name;
+    pokemonHeight.innerText = `La altura es: ${height}`;
+};
+
+getPokemonData();
